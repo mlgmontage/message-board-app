df201cdd06fa1a85dc91a385a759490145fa7238
mlgmontage <kettleofrassel1@gmail.com>
2020-07-08 11:13:39 +0500

    simple pagination

--- backend/api/messages.js
+++ backend/api/messages.js
@@ -4,9 +4,21 @@ const Messages = require('../db/messages')
 
 // get all messages
 router.get('/', async (req, res) => {
-  const messages = await Messages.findAll({})
+  let { limit, offset } = req.query
+
+  limit = parseInt(limit) || 10
+  offset = parseInt(offset) || 0
+
+  // limit the limit
+  limit = Math.min(limit, 40)
+
+  const messages = await Messages.findAll({ limit, offset })
   res.json({
     data: messages,
+    pagination: {
+      limit,
+      offset,
+    },
   })
 })
 
