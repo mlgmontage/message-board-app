52adc64bac14f65a832d9292296e14e7d1dd2269
mlgmontage <kettleofrassel1@gmail.com>
2020-07-08 15:07:37 +0500

    serving static file for vanilla frontend

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const morgan = require('morgan')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
 
 // middlewares
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
 
 // api routes
-app.use('/messages', require('./api/messages'))
+app.use("/messages", require("./api/messages"));
 
-const port = process.env.PORT || 1337
+// static files
+app.use("/vanilla", express.static("../frontend"));
+
+const port = process.env.PORT || 1337;
 app.listen(port, () =>
-  console.log(`Server listenning on http://localhost:${port}`),
-)
+  console.log(`Server listenning on http://localhost:${port}`)
+);
